ce381c4aca8d451e423f0eea5efb6dd877d22cf2
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-01-12 12:03:15 +0100

    first commit

---
+++ script/data_milestone1.js
@@ -0,0 +1,24 @@
+const data = {
+  user: {
+    name: 'Nome Utente',
+    avatar: '_io'
+  },
+  contacts: [
+    {
+      name: 'Michele',
+      avatar: '_1',
+    },
+    {
+      name: 'Fabio',
+      avatar: '_2',
+    },
+    {
+      name: 'Samuele',
+      avatar: '_3',
+    },
+    {
+      name: 'Luisa',
+      avatar: '_4',
+    },
+  ]
+}
